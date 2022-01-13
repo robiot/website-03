c8c2c427360da691331f532fd4a424f8557ebb07
robiot <r0w80abc@gmail.com>
2022-01-13 18:26:54 +0200

    Remove duplicated code

--- src/components/HomePage/Experience/Experience.tsx
+++ src/components/HomePage/Experience/Experience.tsx
@@ -1,5 +1,5 @@
-import { Languages, Other, Technologies } from "../../../lib/experience";
 import { Container } from "../../../style/Container";
+import { Languages, Other, Technologies } from "./experience";
 import {
     ExperienceColumn,
     ExperienceItem,
@@ -10,6 +10,17 @@ import {
     Separator,
 } from "./Experience.style";
 
+const getItems = (itemtype: { name: string; image: string }[]) => {
+    return itemtype.map((item, index) => {
+        return (
+            <ExperienceItem key={index}>
+                <img alt="" src={`/img/experiences/${item.image}`} />
+                {item.name}
+            </ExperienceItem>
+        );
+    });
+};
+
 export const Experience = () => {
     return (
         <Section id="experience">
@@ -18,51 +29,21 @@ export const Experience = () => {
                 <Experiences>
                     <ExperienceColumn>
                         <ExperienceTitle>Languages</ExperienceTitle>
-                        {Languages.map((item, index) => {
-                            return (
-                                <ExperienceItem key={index}>
-                                    <img
-                                        alt=""
-                                        src={`/img/experiences/${item.image}`}
-                                    />
-                                    {item.name}
-                                </ExperienceItem>
-                            );
-                        })}
+                        {getItems(Languages)}
                     </ExperienceColumn>
 
                     <Separator />
 
                     <ExperienceColumn>
                         <ExperienceTitle>Technologies</ExperienceTitle>
-                        {Technologies.map((item, index) => {
-                            return (
-                                <ExperienceItem key={index}>
-                                    <img
-                                        alt=""
-                                        src={`/img/experiences/${item.image}`}
-                                    />
-                                    {item.name}
-                                </ExperienceItem>
-                            );
-                        })}
+                        {getItems(Technologies)}
                     </ExperienceColumn>
 
                     <Separator />
 
                     <ExperienceColumn>
                         <ExperienceTitle>Other</ExperienceTitle>
-                        {Other.map((item, index) => {
-                            return (
-                                <ExperienceItem key={index}>
-                                    <img
-                                        alt=""
-                                        src={`/img/experiences/${item.image}`}
-                                    />
-                                    {item.name}
-                                </ExperienceItem>
-                            );
-                        })}
+                        {getItems(Other)}
                     </ExperienceColumn>
                 </Experiences>
             </Container>
